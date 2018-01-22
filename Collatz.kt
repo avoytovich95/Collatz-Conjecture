174fc76bdd20ce56c33bbcfb5971ee3e63aea283
avoytovich95 <darking271@gmail.com>
2018-01-22 12:22:40 -0500

    Kotlin version

---
+++ Collatz.kt
@@ -0,0 +1,27 @@
+// Collatz Conjecture written in Kotlin
+// Tested in Kotlin sdk
+
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val input = Scanner(System.`in`)
+    print("Enter a positive integer: ")
+    var num = input.nextInt()
+
+    while (num != 1) {
+        print("$num ")
+        if ((num % 2) == 0)
+            num /= 2
+        else 
+            num = num * 3 + 1
+    }
+    println(num)
+}
+
+/* Demo:   
+    Enter a positive integer: 300
+    300 150 75 226 113 340 170 85 256 128 64 32 16 8 4 2 1
+
+    Enter a positive integer: 120
+    120 60 30 15 46 23 70 35 106 53 160 80 40 20 10 5 16 8 4 2 1
+*/
