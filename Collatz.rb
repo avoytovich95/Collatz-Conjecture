0fdf400690378a7200bd18f7e618846f85cb1dce
avoytovich95 <darking271@gmail.com>
2018-01-23 10:34:44 -0500

    Ruby comments fixed

--- Collatz.rb
+++ Collatz.rb
@@ -17,10 +17,11 @@ num = gets.to_i
 collatz(num)
 puts
 
-/ Demo:
+=begin
+Demo:
     Enter a positive integer: 300
     300 150 75 226 113 340 170 85 256 128 64 32 16 8 4 2 1
 
     Enter a positive integer: 120
     120 60 30 15 46 23 70 35 106 53 160 80 40 20 10 5 16 8 4 2 1
-/
+=end
