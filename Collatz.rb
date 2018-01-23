537a26c1706ac79e6f8cc608facd70250b524c5d
avoytovich95 <darking271@gmail.com>
2018-01-23 10:32:30 -0500

    Ruby version

---
+++ Collatz.rb
@@ -0,0 +1,26 @@
+# Collatz Conjecture in Ruby
+# Tested in Ruby Scripter
+
+def collatz(num)
+    print "#{num} "
+    if num > 1
+        if num % 2 == 0
+            collatz(num / 2)
+        else 
+            collatz(num * 3 + 1)
+        end
+    end
+end
+
+print "Enter a positive integer: "
+num = gets.to_i
+collatz(num)
+puts
+
+/ Demo:
+    Enter a positive integer: 300
+    300 150 75 226 113 340 170 85 256 128 64 32 16 8 4 2 1
+
+    Enter a positive integer: 120
+    120 60 30 15 46 23 70 35 106 53 160 80 40 20 10 5 16 8 4 2 1
+/
